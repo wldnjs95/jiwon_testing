54a042bf918f7befe651846fe0c0a3c8636c6b50
wldnjs057 <jiwonpark@utexas.edu>
2023-10-15 04:33:47 -0500

    testing file add

---
+++ pages/jiwontesting.js
@@ -0,0 +1,9 @@
+import styles from './jiwontesting.module.css';
+
+export default function Test() {
+    return (
+        <div className='test-class text-center text-violet-500'>
+            hello
+        </div>
+    )
+}
