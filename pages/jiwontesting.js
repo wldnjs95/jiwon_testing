c80a79af96cc20228372932545e88521d4cde49e
wldnjs95 <jiwonpark@utexas.edu>
2023-11-08 18:42:34 -0600

    delete

--- pages/jiwontesting.js
+++
@@ -1,9 +0,0 @@
-import styles from './jiwontesting.module.css';
-
-export default function Test() {
-    return (
-        <div className='test-class text-center text-violet-500'>
-            hello
-        </div>
-    )
-}
